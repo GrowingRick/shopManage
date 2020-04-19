c64b37cb9c14013ff66e8821753ff0f361e1ed4d
GrowingRick <liziranzr@gmail.com>
2020-04-19 11:04:33 +0800

    operate

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="login_box">
+        <div><img src="../assets/logo.png" alt=""></div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+    .login_box {
+        height: 400px;
+        width: 500px;
+        background-color: #ccc;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-250px, -200px);
+    }
+</style>
